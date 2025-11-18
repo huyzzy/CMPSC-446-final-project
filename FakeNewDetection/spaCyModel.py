69baac208d3e317ce0f9910eb84c40ed15107181
Neville Pochara <77116245+Pillowcaseguy@users.noreply.github.com>
2025-11-17 22:55:55 -0500

    Update spaCyModel.py
    New dependency added, and now we have sentiment analysis using Vader.

--- FakeNewDetection/spaCyModel.py
+++ FakeNewDetection/spaCyModel.py
@@ -1,6 +1,12 @@
 import spacy
 from spacy.tokens import Doc, Span
 from spacy.language import Language
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+#RUN THIS COMMAND IF NEEDED
+#python -m spacy download en_core_web_lg
+
+vader = SentimentIntensityAnalyzer()
 
 EXAGGERATION = {"unbelievable","massive","shocking","incredible","historic"}
 FEAR = {"crisis","collapse","emergency","panic","deadly","catastrophic"}
@@ -9,6 +15,8 @@ BIAS = {"obviously","clearly","must","should","have","need"}
 # Register extension if not already registered
 if not Doc.has_extension("persuasive_spans"):
     Doc.set_extension("persuasive_spans", default=[])
+if not Doc.has_extension("sentiment"):
+    Doc.set_extension("sentiment", default=None)
 
 @Language.component("persuasion_component")
 def persuasion_component(doc):
@@ -23,6 +31,8 @@ def persuasion_component(doc):
             spans.append(("BIAS_MODALITY", token.i, token.i+1))
 
     doc._.persuasive_spans = [Span(doc, s, e, label=l) for l, s, e in spans]
+    sentiment_scores = vader.polarity_scores(doc.text)
+    doc._.sentiment = sentiment_scores
     return doc
 
 def load_spacy():
